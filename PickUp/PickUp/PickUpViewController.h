7f24bf68f8fd4871ec85b00ef26ebb4d36532701
Matthew Steven Pessa <nerdherdknight@gmail.com>
2014-03-06 15:53:41 -0800

    More views added. Linking in work.

---
+++ PickUp/PickUp/PickUpViewController.h
@@ -0,0 +1,17 @@
+//
+//  PickUpViewController.h
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/6/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickUpViewController : UIViewController
+
+- (IBAction)goToMyEvents:(id)sender;
+- (IBAction)createEvent:(id)sender;
+- (IBAction)logout:(id)sender;
+
+@end
---
+++ PickUp/PickUp/PickUpViewController.m
@@ -0,0 +1,46 @@
+//
+//  PickUpViewController.m
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/6/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import "PickUpViewController.h"
+
+@interface PickUpViewController ()
+
+@end
+
+@implementation PickUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goToMyEvents:(id)sender {
+}
+
+- (IBAction)createEvent:(id)sender {
+}
+
+- (IBAction)logout:(id)sender {
+}
+@end
