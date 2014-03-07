c87fdda542ff04473d88cb81ca7bb8802c106128
Matthew Steven Pessa <nerdherdknight@gmail.com>
2014-03-07 14:15:39 -0800

    Links between all views working.

---
+++ PickUp/PickUp/InitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.h
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/7/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialViewController : UIViewController
+
+@end
---
+++ PickUp/PickUp/MyEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyEventsViewController.h
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/7/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyEventsViewController : UITableViewController
+
+@end
