db5114a2b7e16e140edf5532e55f5af747a9599d
Matthew Steven Pessa <nerdherdknight@gmail.com>
2014-03-06 16:13:41 -0800

    Minor changes. Added another view controller.

---
+++ PickUp/PickUp/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/6/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
