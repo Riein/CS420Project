f83dc58cfda20fd3295465557b34b673affc273a
Matthew Steven Pessa <nerdherdknight@gmail.com>
2014-03-11 15:03:55 -0700

    Event info page mostly populated. Button is not appearing yet.

--- PickUp/PickUp/EventInfoViewController.m
+++ PickUp/PickUp/EventInfoViewController.m
@@ -23,24 +23,18 @@
     return self;
 }
 
-//-(void)loadView{
-//    //[super viewDidAppear:animated];
-//    [self.scrollView setScrollEnabled:YES];
-//    [self.scrollView setContentSize:CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height*2)];
-//}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     _scrollView = [[UIScrollView alloc] init];
     _scrollView.frame = self.view.frame;
-    [self.scrollView setContentSize:CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height*2)];
+    self.scrollView.ContentSize = CGSizeMake(320, 720);
     [self.scrollView setScrollEnabled:YES];
+    _insideView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 720)];
+    [self.scrollView addSubview:self.insideView];
     [self.view addSubview:self.scrollView];
-    [self.scrollView layoutSubviews];
-    [self.scrollView setNeedsDisplay];
     [self.view setBackgroundColor:[UIColor whiteColor]];
-    _mapView = [[MKMapView alloc] init];
+    _mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, 25, 320, 200)];
     NSNumber *latitude = [self.info objectForKey:@"Latitude"];
     NSNumber *longitude = [self.info objectForKey:@"Longitude"];
     _region.center.latitude = latitude.doubleValue;
@@ -48,25 +42,49 @@
     _region.span.latitudeDelta = 0.02;
     _region.span.longitudeDelta = 0.02;
     _mapView.region = _region;
+    [self.scrollView addSubview:_mapView];
     NSLog(@"mapView.region set");
     // Does not work with or without the following commented section
-/*    [self.scrollView addSubview:_mapView];
-    _locField = [[UITextField alloc] initWithFrame:CGRectMake(114, 172, 206, 30)];
+
+    _locField = [[UITextField alloc] initWithFrame:CGRectMake(92, 288, 206, 30)];
+    _locField.borderStyle = UITextBorderStyleBezel;
+    [_locField setEnabled:NO];
     [self.scrollView addSubview:_locField];
-    _dateField = [[UITextField alloc] init];
+    UILabel *locLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 292, 67, 21)];
+    locLabel.text = @"Location";
+    [self.scrollView addSubview:locLabel];
+    _dateField = [[UITextField alloc] initWithFrame:CGRectMake(92, 331, 206, 30)];
+    _dateField.borderStyle = UITextBorderStyleBezel;
+    [_dateField setEnabled:NO];
     [self.scrollView addSubview:_dateField];
-    _timeField = [[UITextField alloc] init];
+    UILabel *dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 335, 67, 21)];
+    dateLabel.text = @"Date";
+    [self.scrollView addSubview:dateLabel];
+    _timeField = [[UITextField alloc] initWithFrame:CGRectMake(92, 374, 206, 30)];
+    _timeField.borderStyle = UITextBorderStyleBezel;
+    [_timeField setEnabled:NO];
     [self.scrollView addSubview:_timeField];
-    _equipField = [[UITextView alloc] init];
+    UILabel *timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 378, 67, 21)];
+    timeLabel.text = @"Time";
+    [self.scrollView addSubview:timeLabel];
+    _equipField = [[UITextView alloc] initWithFrame:CGRectMake(41, 516, 239, 65)];
+    [_equipField setBackgroundColor:[UIColor lightGrayColor]];
+    [_equipField setEditable:NO];
     [self.scrollView addSubview:_equipField];
-    _button = [[UIButton alloc] init];
+    UILabel *equipLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 487, 162, 21)];
+    equipLabel.text = @"Required Equipment:";
+    [self.scrollView addSubview:equipLabel];
+    
+    // Button not being added still. Need to figure out why.
+    _button = [[UIButton alloc] initWithFrame:CGRectMake(115, 610, 90, 34)];
+    [_button setTitle:@"Join" forState:UIControlStateApplication];
     [self.scrollView addSubview:_button];
-    UILabel *testLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 60, 100, 50)];
-    testLabel.text = @"testing";
-    [self.scrollView addSubview:testLabel]; */
+
     NSLog(@"viewDidLoad");
   
-    //[self.view sendSubviewToBack:_scrollView];
+    [self.scrollView layoutSubviews];
+    [self.scrollView setNeedsDisplay];
+    [self.view setNeedsDisplay];
 }
 
 - (void)didReceiveMemoryWarning
@@ -75,8 +93,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)joinButton:(id)sender {
-
-}
 
 @end
