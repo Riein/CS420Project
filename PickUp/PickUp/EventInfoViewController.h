ebbdd0479b14e7d8bc4a5979b216afd1985f29a1
Matthew Steven Pessa <nerdherdknight@gmail.com>
2014-03-11 12:01:35 -0700

    Attempting to fix scroll view issue. Scroll view visible but nothing in it.

--- PickUp/PickUp/EventInfoViewController.h
+++ PickUp/PickUp/EventInfoViewController.h
@@ -11,14 +11,14 @@
 
 @interface EventInfoViewController : UIViewController
 
-@property (assign, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @property MKCoordinateRegion region;
 
-@property (weak, nonatomic) IBOutlet UITextField *locField;
-@property (weak, nonatomic) IBOutlet UITextField *dateField;
-@property (weak, nonatomic) IBOutlet UITextField *timeField;
-@property (weak, nonatomic) IBOutlet UITextView *equipField;
-@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) IBOutlet UITextField *locField;
+@property (strong, nonatomic) IBOutlet UITextField *dateField;
+@property (strong, nonatomic) IBOutlet UITextField *timeField;
+@property (strong, nonatomic) IBOutlet UITextView *equipField;
+@property (strong, nonatomic) IBOutlet UIButton *button;
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property (weak, nonatomic) NSDictionary *info;
--- PickUp/PickUp/EventInfoViewController.m
+++ PickUp/PickUp/EventInfoViewController.m
@@ -32,11 +32,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _scrollView = [[UIScrollView alloc] init];
+    _scrollView.frame = self.view.frame;
     [self.scrollView setContentSize:CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height*2)];
+    [self.scrollView setScrollEnabled:YES];
+    [self.view addSubview:self.scrollView];
+    [self.scrollView layoutSubviews];
     [self.scrollView setNeedsDisplay];
-    NSLog(@"viewDidLoad");
-    // Do any additional setup after loading the view from its nib.
-    //MKMapView *mapView = [[MKMapView alloc]]
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    _mapView = [[MKMapView alloc] init];
     NSNumber *latitude = [self.info objectForKey:@"Latitude"];
     NSNumber *longitude = [self.info objectForKey:@"Longitude"];
     _region.center.latitude = latitude.doubleValue;
@@ -45,8 +49,23 @@
     _region.span.longitudeDelta = 0.02;
     _mapView.region = _region;
     NSLog(@"mapView.region set");
-    
-    
+//    [self.scrollView addSubview:_mapView];
+//    _locField = [[UITextField alloc] initWithFrame:CGRectMake(114, 172, 206, 30)];
+//    [self.scrollView addSubview:_locField];
+//    _dateField = [[UITextField alloc] init];
+//    [self.scrollView addSubview:_dateField];
+//    _timeField = [[UITextField alloc] init];
+//    [self.scrollView addSubview:_timeField];
+//    _equipField = [[UITextView alloc] init];
+//    [self.scrollView addSubview:_equipField];
+//    _button = [[UIButton alloc] init];
+//    [self.scrollView addSubview:_button];
+//    UILabel *testLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 60, 100, 50)];
+//    testLabel.text = @"testing";
+//    [self.scrollView addSubview:testLabel];
+    NSLog(@"viewDidLoad");
+  
+    [self.view sendSubviewToBack:_scrollView];
 }
 
 - (void)didReceiveMemoryWarning
