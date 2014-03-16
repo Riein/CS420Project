8a8de33623005c3511665c3476a74e7962b40f67
Jenny Langlois <jennyklanglois@yahoo.com>
2014-03-15 18:41:15 -0700

    Event info page complete. Needs visual fix still. Button added and works. Minor adjustments might still be needed.

--- PickUp/PickUp/EventInfoViewController.m
+++ PickUp/PickUp/EventInfoViewController.m
@@ -28,7 +28,7 @@
     [super viewDidLoad];
     _scrollView = [[UIScrollView alloc] init];
     _scrollView.frame = self.view.frame;
-    self.scrollView.ContentSize = CGSizeMake(320, 720);
+    self.scrollView.ContentSize = CGSizeMake(320, 800);
     [self.scrollView setScrollEnabled:YES];
     _insideView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 720)];
     [self.scrollView addSubview:self.insideView];
@@ -44,7 +44,6 @@
     _mapView.region = _region;
     [self.scrollView addSubview:_mapView];
     NSLog(@"mapView.region set");
-    // Does not work with or without the following commented section
 
     _locField = [[UITextField alloc] initWithFrame:CGRectMake(92, 288, 206, 30)];
     _locField.borderStyle = UITextBorderStyleBezel;
@@ -81,13 +80,8 @@
     UILabel *playersLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 421, 67, 21)];
     playersLabel.text = @"Players:";
     [self.insideView addSubview:playersLabel];
+
     
-    // Button not being added still. Need to figure out why. Thinking of just putting button in top bar.
-    _button = [[UIButton alloc] initWithFrame:CGRectMake(115, 610, 90, 34)];
-    [_button setTitle:@"Join" forState:UIControlStateApplication];
-    [self.insideView addSubview:_button];
-    
-    // Date not wanting to show up. I think because the date is it's own type, not a string.
     NSDateFormatter *day = [[NSDateFormatter alloc] init];
     [day setDateFormat:@"MM/dd/yyyy"];
     NSString *date = [day stringFromDate:[self.info objectForKey:@"Date"]];
@@ -111,12 +105,28 @@
         [equip appendString:@"\n"];
     }
     _equipField.text = equip;
+    
+    _button = [[UIButton alloc] initWithFrame:CGRectMake(115, 610, 90, 34)];
+    [_button setTitle:@"Join" forState:UIControlStateNormal];
+    // Add this in later, change the button title if already joined
+//    for (int i = 0; i < ppl.count; i++) {
+//        if (currentUser == ppl[i]) {
+//            [_button setTitle:@"Unjoin" forState:UIControlStateApplication];
+//        }
+//    }
+    [_button setBackgroundColor:[UIColor lightGrayColor]];
+    [_button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [_button addTarget:self
+                 action:@selector(buttonPressed:)
+       forControlEvents:UIControlEventTouchUpInside];
+    [self.insideView addSubview:_button];
 
     NSLog(@"viewDidLoad");
   
     [self.scrollView layoutSubviews];
     [self.scrollView setNeedsDisplay];
     [self.view setNeedsDisplay];
+    self.scrollView.delaysContentTouches = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -125,5 +135,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)touchesShouldCancelInContentView:(UIView *)view {
+    return NO;
+}
+
+-(void)buttonPressed:(id)sender{
+    NSLog(@"button pressed");
+    // Going to add user to list, send update to server, change button title
+}
 
 @end
