c037d7b4f897cbb42b77ba6b0ee3397ea5fa038f
Jenny Langlois <jennyklanglois@yahoo.com>
2014-03-22 09:31:06 -0700

    Created model. No work done in model yet.

---
+++ PickUp/PickUp/PickUp.h
@@ -0,0 +1,15 @@
+//
+//  PickUp.h
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/22/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PickUp : NSObject
+
+@property (strong, nonatomic) NSString *user;
+
+@end
---
+++ PickUp/PickUp/PickUp.m
@@ -0,0 +1,13 @@
+//
+//  PickUp.m
+//  PickUp
+//
+//  Created by Matthew Steven Pessa on 3/22/14.
+//  Copyright (c) 2014 Amnesiacs. All rights reserved.
+//
+
+#import "PickUp.h"
+
+@implementation PickUp
+
+@end
